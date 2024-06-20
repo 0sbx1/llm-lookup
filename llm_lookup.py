57bba56fa1531b30738ac06c7199eb47515cec0e
Max Karreth <max@karreth.com>
2024-06-20 15:16:58 -0400

    Small changes

--- llm_lookup.py
+++ llm_lookup.py
@@ -28,7 +28,7 @@ def lookup(df1, df2, join1, join2, matching_context="",openai_model='gpt-3.5-tur
                     prompt = f"Using the best of your knowledge and precise data analysis skills, for the value \"{df1_value}\" of column \"{join1}\", please choose the most appropriate value from the list from column \"{join2}\" below. Return only the exact value as given with no additional text. Important, if you do not see a fitting result, return N/A. Values are: {chunk}"
                 else:
                     prompt = f"{matching_context}. Using the best of your knowledge and precise data analysis skills, for the value \"{df1_value}\" of column \"{join1}\", please choose the most appropriate value from the list from column \"{join2}\" below. Return only the exact value as given with no additional text. Important, if you do not see a fitting result, return N/A. Values are: {chunk}"
-                response = openai.chat.completions.create(
+                    response = openai.chat.completions.create(
                     model=openai_model,
                     messages=[
                         {"role": "user",
